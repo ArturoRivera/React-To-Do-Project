0df628cc606c97823bf726bd7c163ed3c5dc4c86
Arturo Rivera <a.rivera13@hotmail.com>
2018-12-27 16:39:39 -0800

    Completed Components for To Do App

---
+++ src/components/ToDo.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ToDo extends Component {
+    render(){
+        return (
+            <li>A To Do will go here</li>
+        );
+    }
+}
+
+export default ToDo;
